b606b4189ad40866b2ea934a4605f07f3e04ac3e
devpenzil <ajoalex012@gmail.com>
2022-01-25 16:46:31 +0530

    email table

--- hbstemplate/routes/index.js
+++ hbstemplate/routes/index.js
@@ -2,8 +2,11 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
   res.render('index', { uname: 'John Deo' });
 });
 
+router.get('/email', (req, res, next) => {
+  res.render('email',{name: 'John Deo'})
+})
 module.exports = router;
